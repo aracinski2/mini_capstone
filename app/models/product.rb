b5995c9fcf75c78076c9bc801f74615ec5f3a07d
Alex Racinski <aracinski2@gmail.com>
2020-11-11 18:25:47 -0600

    add discount and total methods to index and show action

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ApplicationRecord
   def tax
     price * 0.09
-  end   
+  end
+
+  def is_discounted
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def total
+    price + tax
+  end
 end
