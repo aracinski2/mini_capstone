33bbc8b32afbb980238bcae8cf4049d9639552c6
Alex Racinski <aracinski2@gmail.com>
2020-11-16 20:38:58 -0600

    add / to patch delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
-    patch "products/:id" => "products#update"
-    delete "products/:id`" => "products#delete"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id`" => "products#delete"
+    
+    # get "suppliers"
   end
   
 end
