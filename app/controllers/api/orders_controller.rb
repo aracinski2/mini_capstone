23b9cc870f95c8a8de6e593342c930c9e821897f
Alex Racinski <aracinski2@gmail.com>
2020-11-19 19:36:00 -0600

    add order create action with route and show view

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,11 @@
 class Api::OrdersController < ApplicationController
+  def create
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity]
+    )
+    # @order.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
 
     post "/users" => "users#create"
     post "/sessions" => "sessions#create"
+
+    post "/orders" => "orders#create"
   end
   
 end
