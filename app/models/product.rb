032251fb2113785e994e3ae75d0db76efe109527
Alex Racinski <aracinski2@gmail.com>
2020-11-23 19:21:54 -0600

    add has many, through association to category and product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ApplicationRecord
   has_many :orders
 
   has_many :category_products
+  has_many :categories, through: :category_products
 
   # add if/ else condition to create/update action
 
