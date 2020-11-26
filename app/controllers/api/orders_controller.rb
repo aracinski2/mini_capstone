760e0b7857e3b0e98cad2b41d822b88700d0574e
Alex Racinski <aracinski2@gmail.com>
2020-11-26 15:11:40 -0600

    update order create action to calculate subtotal, tax, total

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,19 +7,29 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    subtotal = product.price * params[:quantity].to_i
-    tax_value = 0.09
-    tax = subtotal * tax_value
+    # product = Product.find_by(id: params[:product_id])
+    # subtotal = product.price * params[:quantity].to_i
+    # tax_value = 0.09
+    # tax = subtotal * tax_value
+    carted_products = current_user.carted_products.where(status: "carted")
     
-    
+    subtotal = 0
+    carted_products.each do |carted_product|
+      subtotal += carted_product.product.price * carted_product.quantity
+    end
+    tax_rate = 0.09
+    tax = subtotal * tax_rate
+    total = tax + subtotal
+
+
     @order = Order.new(
       user_id: current_user.id,
       subtotal: subtotal,
       tax: tax,
-      total: subtotal + tax
-    )
+      total: total
+    ) 
     @order.save
+    
     render 'show.json.jb'
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
-  belongs_to :product
 
   has_many :carted_products
 end
